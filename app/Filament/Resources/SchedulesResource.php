6bd6e1359bd084c0804c627fbed3baac727e53a2
Khairul Umam Albi <albikhairul212@gmail.com>
2024-12-22 23:25:41 +0700

    add: Filament, Model, and Migration

---
+++ app/Filament/Resources/SchedulesResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SchedulesResource\Pages;
+use App\Filament\Resources\SchedulesResource\RelationManagers;
+use App\Models\Schedules;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SchedulesResource extends Resource
+{
+    protected static ?string $model = Schedules::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->searchable()
+                    ->required(),
+                Forms\Components\Select::make('shift_id')
+                    ->relationship('shifts', 'name')
+                    ->required(),
+                Forms\Components\Select::make('office_id')
+                    ->relationship('offices', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('shift_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('office_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchedules::route('/'),
+            'create' => Pages\CreateSchedules::route('/create'),
+            'edit' => Pages\EditSchedules::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ php.sh
@@ -0,0 +1,4 @@
+# make ssh connection to the server and run the php script with port 3000
+
+php artisan serve --port=3000
+
